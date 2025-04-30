4c0fe713da113334e1fc2b6d11b90c6ee548b5bb
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-30 01:02:09 -0300

    feat: servidor ativo

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.send("API - Encenna");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ativo na porta ${PORT}...`);
+});
