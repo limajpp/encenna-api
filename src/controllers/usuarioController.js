b4506d3f410466d1fc85a7dbd58550350c4452e9
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-30 01:52:33 -0300

    feat: crio controller do usuário

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,22 @@
+import Usuario from "../models/usuario.js";
+
+const criarUsuario = async (req, res) => {
+  try {
+    const { nome, email, senhaHash, telefone, cpf, tipo, fotoPerfil } =
+      req.body;
+    const novoUsuario = new Usuario({
+      nome,
+      email,
+      senhaHash,
+      telefone,
+      cpf,
+      tipo,
+      fotoPerfil,
+    });
+    await novoUsuario.save();
+    res.send(201).json(novoUsuario);
+    console.log("Usuário criado com sucesso!");
+  } catch (error) {
+    console.error("Algo deu errado ao tentar criar um novo usuário...", error);
+  }
+};
