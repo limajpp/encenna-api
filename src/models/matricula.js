8ea0a06faaf14e1cf1b6741577e54ab20a23e7ef
João Pedro Lima <joaopedrocal90@gmail.com>
2025-05-01 17:56:49 -0300

    feat: adiciono valor min e max de nota e frequencia no controller

--- src/models/matricula.js
+++ src/models/matricula.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const matriculaSchema = mongoose.Schema({
   alunoId: { type: mongoose.Types.ObjectId, required: true },
   turmaId: { type: mongoose.Types.ObjectId, required: true },
-  nota: { type: Number, required: true },
-  frequencia: { type: Number, required: true },
+  nota: { type: Number, required: true, min: 0, max: 10 },
+  frequencia: { type: Number, required: true, min: 0, max: 100 },
 });
 
 export default mongoose.model("Matricula", matriculaSchema, "matriculas");
