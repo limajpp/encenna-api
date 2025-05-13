5d54cb46a69cbf563cf52a315108708cf4e48fa2
João Pedro Lima <joaopedrocal90@gmail.com>
2025-05-13 14:49:40 -0300

    fix: port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor ativo na porta ${PORT}...`);
